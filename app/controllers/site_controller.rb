5ba30417e30eaedd9758cc7a375cffd2687ea91b
Jimin Wen <jiminwen@tamu.edu>
2016-11-04 20:59:00 +0000

    formM_1

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -122,6 +122,24 @@ class SiteController < ApplicationController
     return @query
   end
   
+   def formF_4
+      year=params[:year]
+      last_year=year.to_i-1 
+      last_year=last_year.to_s
+      #raise params.inspect
+      @students=Student.all
+      @students=@students.where("prim_deg='PHD'").where("year=?",year)
+    
+      c_attribute=[" ","CS","CE"]
+      r_attribute=["Number of PHD students from outside the North America?","Prior Year"]
+      c_filter=["prim_deg_maj_1 = 'CPSL' OR prim_deg_maj_1 = 'CPSC'","prim_deg_maj_1 = 'CECN' OR prim_deg_maj_1 = 'CECL'"]
+      r_filter=["country_of_origin!= ' ' AND country_of_origin!= 'United States'"]
+      
+      respond_to do |format|
+        format.csv { send_data Student.csv_table(@students, c_filter,r_filter,c_attribute,r_attribute) }
+      end
+  end
+  
   def fromI_1
       year=params[:year]
       @students=Student.all
@@ -242,22 +260,49 @@ class SiteController < ApplicationController
       end
   end
   
-  def formF_4
+ 
+  def fromM_1
       year=params[:year]
-      last_year=year.to_i-1 
-      last_year=last_year.to_s
-      #raise params.inspect
       @students=Student.all
-      @students=@students.where("prim_deg='PHD'").where("year=?",year)
-    
-      c_attribute=[" ","CS","CE"]
-      r_attribute=["Number of PHD students from outside the North America?","Prior Year"]
-      c_filter=["prim_deg_maj_1 = 'CPSL' OR prim_deg_maj_1 = 'CPSC'","prim_deg_maj_1 = 'CECN' OR prim_deg_maj_1 = 'CECL'"]
-      r_filter=["country_of_origin!= ' ' AND country_of_origin!= 'United States'"]
+      #raise @students.inspect
+      @students=@students.where("prim_deg_maj_1='CPSC' OR prim_deg_maj_1='CPSL'").where("prim_deg='BS'").where("year=?",year)
+      c_attribute=[" ","Male","Female","Not Avaliable","Total"]
+      r_attribute=["Residents (a.-h.)","a. American Indian or Alaska Native, not Hispanic", "b. Asain, not Hispanic","c. Black or African-American, not Hispanic","d. Native Hawallian or Other Pacific Islander, not Hispanic","e. White, not Hispanic"]
+      r_attribute=r_attribute+["f. More than one race, not Hispanic","g. Hispanic or Latino, any race", "h. Race/Ethnicity Unknown","i. Nonresident Alien","j. Not available","k. Total"]
+      c_filter=["sex='M'","sex='F'","sex=' '","sex='M' OR sex='F' OR sex=' '"]
+      r_filter=["residency='R' AND ethnicity='I'","residency='R' AND ethnicity='T'","residency='R' AND ethnicity='B'","residency='R' AND ethnicity='N'"] 
+      r_filter=r_filter+["residency='R' AND ethnicity='W'","residency='R' AND ethnicity='M'","residency='R' AND ethnicity='H'","residency='R' AND ethnicity='O'"]
+      r_filter=r_filter+["residency='U'","residency!='R' AND residency!='U'"]
+     
+      
+      array=[]
+      array<<c_attribute
+      temp=[r_attribute[0]," "," "," "," "]
+      array<<temp
+      i=1
+      r_filter.each do |r|
+        temp=[]
+        temp<<r_attribute[i]
+        i=i+1
+        
+        c_filter.each do |c|
+        temp<< @students.where(c).where(r).count.to_s
+        end
+        
+        array<<temp
+      end
+      temp=["k. Total"]
+      c_filter.each do |c|
+      temp<< @students.where(c).count.to_s
+      end
+      array<<temp
       
       respond_to do |format|
-        format.csv { send_data Student.csv_table(@students, c_filter,r_filter,c_attribute,r_attribute) }
+      
+        format.csv { send_data Student.altercsv(array) }
+       
       end
+      
   end
   
   
