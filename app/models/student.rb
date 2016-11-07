56629c4621bb84b3848cd1d7ac2cb9252f87809b
Fanchao Zhou <fanchaozhou1992@gmail.com>
2016-11-07 21:10:42 +0000

    Added the final two forms

--- app/models/student.rb
+++ app/models/student.rb
@@ -20,6 +20,46 @@ class Student < ActiveRecord::Base
         end
     end
     
+    def self.to_j1_csv(year)
+        puts "hello"
+        puts year
+        puts year.class
+        
+        new_students = {}
+        new_students[ "CP" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg like ?", "Summer "+year.to_s+"%", "Fall "+year.to_s+"%", "Spring "+(year+1).to_s+"%", "CP%", "M%" ]).count
+        new_students[ "CE" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg like ?", "Summer "+year.to_s+"%", "Fall "+year.to_s+"%", "Spring "+(year+1).to_s+"%", "CE%", "M%" ]).count
+    
+        prior_students = {}
+        prior_students[ "CP" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg like ?", "Summer "+(year-1).to_s+"%", "Fall "+(year-1).to_s+"%", "Spring "+year.to_s+"%", "CP%", "M%" ]).count
+        prior_students[ "CE" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg like ?", "Summer "+(year-1).to_s+"%", "Fall "+(year-1).to_s+"%", "Spring "+year.to_s+"%", "CE%", "M%" ]).count
+        
+        CSV.generate do |csv|
+            csv << ["", "CS", "CE"]
+            csv << ["Number of newly-admitted masters students", new_students[ "CP" ].to_s, new_students[ "CE" ].to_s]
+            csv << ["Prior Year", prior_students[ "CP" ].to_s, prior_students[ "CE" ].to_s]
+        end
+    end
+    
+    def self.to_f3_csv(year)
+        puts "hello"
+        puts year
+        puts year.class
+        
+        new_students = {}
+        new_students[ "CP" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg = ?", "Summer "+year.to_s+"%", "Fall "+year.to_s+"%", "Spring "+(year+1).to_s+"%", "CP%", "PHD" ]).count
+        new_students[ "CE" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg = ?", "Summer "+year.to_s+"%", "Fall "+year.to_s+"%", "Spring "+(year+1).to_s+"%", "CE%", "PHD" ]).count
+        
+        prior_students = {}
+        prior_students[ "CP" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg = ?", "Summer "+(year-1).to_s+"%", "Fall "+(year-1).to_s+"%", "Spring "+year.to_s+"%", "CP%", "PHD" ]).count
+        prior_students[ "CE" ] = where([ "(prim_deg_cat like ? or prim_deg_cat like ? or prim_deg_cat like ?) and prim_deg_maj_1 like ? and prim_deg = ?", "Summer "+(year-1).to_s+"%", "Fall "+(year-1).to_s+"%", "Spring "+year.to_s+"%", "CE%", "PHD" ]).count
+        
+        CSV.generate do |csv|
+            csv << ["", "CS", "CE"]
+            csv << ["Number of newly admitted PhD Students", new_students[ "CP" ].to_s, new_students[ "CE" ].to_s]
+            csv << ["Number of PhD Students Prior Year", prior_students[ "CP" ].to_s, prior_students[ "CE" ].to_s]
+        end
+    end
+    
     def self.csv_table(product,c_filter,r_filter,c_attri,r_attri)
         CSV.generate do |csv|
          #   raise params.inspect
